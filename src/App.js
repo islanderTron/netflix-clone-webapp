ba79294ab7fcb553408f3934848ce6db7c6bf091
islandertron <islandertron@gmail.com>
2020-09-24 22:19:46 -0400

    Implemented aws cli and deploy

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <h1>Test, deployed</h1>
     </div>
   );
 }
