df108b823afe01934b17c600a30723dfdd02e3b9
islandertron <islandertron@gmail.com>
2021-04-08 08:00:26 -0400

    - Added Router library - Maintained the url path

--- src/App.js
+++ src/App.js
@@ -2,18 +2,32 @@ import React from 'react';
 
 import './css/App.css';
 
+// Components
+import Home from './components/Home';
 import Navigation from './components/nav/navigation';
 import Video from './components/video/Video';
 
+// Router
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      {/* <Navigation /> */}
-      <Video 
-        videoPath="/videos/demo.mp4"
-      />
-    </div>
-  );
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/netflix" exact>
+          <Video videoPath='/videos/demo.mp4' />
+        </Route>
+      </Switch>
+    </Router>
+  )
 }
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Home(props) {
+  console.log(props)
+  return (
+    <h1>Paul J. Uncangco</h1>
+  )
+}
+
+export default Home; 
